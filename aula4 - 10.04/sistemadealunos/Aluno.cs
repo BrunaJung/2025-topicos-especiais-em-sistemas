f5c9791b72ae5a480bd2a34dc1f9dd042556893e
BRUNA MARA JUNG <bruna.jung@cs.up.edu.br>
2025-04-10 10:46:53 -0300

    Exercicio classe de Alunos

---
+++ aula4 - 10.04/sistemadealunos/Aluno.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace SISTEMADEALUNOS{
+    public class Aluno{
+        public string Nome{get; set;}
+        public double Nota1{get; set;}
+        public double Nota2{get; set;}
+
+        public Aluno(string nome, double nota1, double nota2){
+            Nome = nome;
+            Nota1 = nota1;
+            Nota2 = nota2;    
+        }
+
+        public double CalcularMedia(){
+            return (Nota1 + Nota2) / 2;
+        }
+
+        public void ExibirResultado(){
+            double media = CalcularMedia();
+            Console.Write($"\nAluno: {Nome}");
+            Console.Write($"\nMedia: {media}");
+        
+            if(media >= 6.0){
+                Console.Write("\nStatus: APROVADO!");
+            }
+            else{
+                Console.Write("\nStatus: REPROVADO!");
+            }
+        }
+    }
+}
---
+++ aula4 - 10.04/sistemadealunos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace SISTEMADEALUNOS{
+    class Program{
+        static void Main(string[] args){
+            Console.Write("Digite o nome do aluno: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite a primeira nota: ");
+            double nota1 = double.Parse(
+                Console.ReadLine(), CultureInfo.InvariantCulture
+            );
+
+            Console.Write("Digite a segunda nota: ");
+            double nota2 = double.Parse(
+                Console.ReadLine(), CultureInfo.InvariantCulture
+            );
+
+            Aluno aluno = new Aluno(nome, nota1, nota2);
+            aluno.ExibirResultado();
+
+        }
+
+    }
+}
