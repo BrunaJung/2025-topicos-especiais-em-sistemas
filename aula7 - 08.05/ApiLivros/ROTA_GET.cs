77381c1e6f666cb476b5693e6c49a33eb2044318
BRUNA MARA JUNG <bruna.jung@cs.up.edu.br>
2025-05-15 10:49:03 -0300

    exemplo professor livros citação

--- aula7 - 08.05/ApiLivros/ROTA_GET.cs
+++ aula7 - 08.05/ApiLivros/ROTA_GET.cs
@@ -2,27 +2,27 @@ using ApiLivros.Models;
 
 namespace ApiLivros.Routes;
 
-public static void class ROTA_GET{
+public static class ROTA_GET{
 
     public static void MapGetRoutes(this WebApplication app){
-        List<Livros> livros = new List<Livros>
+        List<Livro> livros = new List<Livro>
         {
-            new Livros{Id = 1, Titulo = "Dom Casmurro", Autor = "Machado de Assis", AnoPublicacao = 1899},
-            new Livros{Id = 2, Titulo = "1984", Autor = "George Orwell", AnoPublicacao = 1949},
-            new Livros{Id = 3, Titulo = "O hobbit", Autor = "J. R. R. Tolkien", AnoPublicacao = 1937}
+            new Livro{Id = 1, Titulo = "Dom Casmurro", Autor = "Machado de Assis", AnoPublicacao = 1899},
+            new Livro{Id = 2, Titulo = "1984", Autor = "George Orwell", AnoPublicacao = 1949},
+            new Livro{Id = 3, Titulo = "O hobbit", Autor = "J. R. R. Tolkien", AnoPublicacao = 1937}
         };
 
         //rota get raiz
         app.MapGet("/", () => "Api de livros está em funcionamento");
 
         //rota get - listar todos os livros
-        app.MapGet("api/livros", () => livros);
+        app.MapGet("/api/livros", () => livros);
 
         //rota get - buscar livro por id
         app.MapGet("/api/livros/{id()", (int id) =>
         {
             var livro = livros.FirstOrDefault(l => l.Id == id);
-            return livros != null ? Results.Ok (livros): Results.NotFound ("Livro não encontrado.");
+            return livro != null ? Results.Ok (livro): Results.NotFound ("Livro não encontrado.");
         });
     }
 }
